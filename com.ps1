e5d8ee9c94ed1092a5efcb6b58a92c3830112dd0
T0 <53048989+y5s5k5@users.noreply.github.com>
2021-09-15 16:12:59 +0800

    Update com.ps1

--- com.ps1
+++ com.ps1
@@ -1,4 +1,4 @@
-#放到OleViewDotNet所在目录，查看medium可以访问的system接口
+#放到OleViewDotNet.exe所在目录，查看medium可以访问的system接口
 .\Startup-Module.ps1
 $comDb = Get-ComDatabase -PassThru
 Set-ComDatabase -Path ComDb_old.db -Database $comDb 
@@ -8,9 +8,11 @@ $comsAsSystem  | Select-ComAccess -ProcessId (Get-Process -Name explorer).Id  | 
 
 #放到OleViewDotNet所在目录，查看low可以访问的medium接口
 #.\Startup-Module.ps1
-#$comDb = Get-ComDatabase -PassThru
-#Set-ComDatabase -Path ComDb_old.db -Database $comDb 
-#$comsAsSystem = $comDb.AppIDs.Values | Where-Object -FilterScript { $_.IsService -eq $True -or $_.RunAs -ieq "nt authority\system" }
+#Get-ComDatabase -SetCurrent
+#Set-ComDatabase .\com.db
+#Get-ComDatabase .\com.db -SetCurrent
+#$comdb = Get-CurrentComDatabase
+#$comsAsSystem = Get-ComClass -ServerType LocalServer32 | ? RunAs -eq "Interactive User"
 #$comsAsSystem  | Select-ComAccess -ProcessId  这里输入low权限进程的id | Select Name  >>medium.txt
 
 
