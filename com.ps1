7fec1674af9f59700b5aca9aed15f7d7cbe3802f
T0 <53048989+y5s5k5@users.noreply.github.com>
2021-09-15 16:04:49 +0800

    Create com.ps1

---
+++ com.ps1
@@ -0,0 +1,21 @@
+#放到OleViewDotNet所在目录，查看medium可以访问的system接口
+.\Startup-Module.ps1
+$comDb = Get-ComDatabase -PassThru
+Set-ComDatabase -Path ComDb_old.db -Database $comDb 
+$comsAsSystem = $comDb.AppIDs.Values | Where-Object -FilterScript { $_.IsService -eq $True -or $_.RunAs -ieq "nt authority\system" }
+$comsAsSystem  | Select-ComAccess -ProcessId (Get-Process -Name explorer).Id  | Select Name  >>system.txt
+
+
+#放到OleViewDotNet所在目录，查看low可以访问的medium接口
+#.\Startup-Module.ps1
+#$comDb = Get-ComDatabase -PassThru
+#Set-ComDatabase -Path ComDb_old.db -Database $comDb 
+#$comsAsSystem = $comDb.AppIDs.Values | Where-Object -FilterScript { $_.IsService -eq $True -or $_.RunAs -ieq "nt authority\system" }
+#$comsAsSystem  | Select-ComAccess -ProcessId  这里输入low权限进程的id | Select Name  >>medium.txt
+
+
+#https://www.tiraniddo.dev/2018/09/
+#https://the-deniss.github.io/posts/2021/05/17/discovering-and-exploiting-mcafee-com-objects.html
+#https://vimeo.com/214856542
+
+
